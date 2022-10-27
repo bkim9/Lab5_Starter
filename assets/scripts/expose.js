28e5cdc9080f2e64022345b3ac9927600dad5bfd
bkim9 <bek001@ucsd.edu>
2022-10-27 14:59:09 -0700

    update image and sound file done
    how to play audio file

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -2,6 +2,22 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+
 function init() {
-  // TODO
+  const hornSelect= document.querySelector('#horn-select');
+  const hornImg = document.querySelector('header + img');
+  const volumeIcon = document.querySelector('input + img');
+  const audioE = document.querySelectorAll('.hidden')[0];
+  const volumeControlE = document.querySelector('#volume-controls');
+  const volumeInputE = volumeControlE.children[0];
+  const volumeIconE = volumeControlE.children[1];
+  const playBttnE = document.querySelector('#volume-controls + button');
+  hornSelect.addEventListener('change', () => {
+    hornImg.src = './assets/images/'+ hornSelect.value +'.svg';
+    audioE.src = './assets/audio/'+ hornSelect.value + '.mp3';
+    
+   });
+  playBttnE.addEventListener('click', function w() {
+    audioE.play();
+   });
 }
