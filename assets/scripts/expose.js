bab936497921b5f1f091b59e331b7138bdb72629
bkim9 <bek001@ucsd.edu>
2022-10-27 19:46:14 -0700

    confetti done

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -12,12 +12,31 @@ function init() {
   const volumeInputE = volumeControlE.children[0];
   const volumeIconE = volumeControlE.children[1];
   const playBttnE = document.querySelector('#volume-controls + button');
+  var level = 0;
+  // const jsConfetti = new jsConfetti();
+  const jsConfetti = new JSConfetti();
+
+
   hornSelect.addEventListener('change', () => {
     hornImg.src = './assets/images/'+ hornSelect.value +'.svg';
     audioE.src = './assets/audio/'+ hornSelect.value + '.mp3';
     
    });
-  playBttnE.addEventListener('click', function w() {
+  playBttnE.addEventListener('click', function playHorn() {
     audioE.play();
+    if (hornSelect.value == 'party-horn') { jsConfetti.addConfetti();}
    });
+  volumeInputE.addEventListener('input', function inputChage() {
+    audioE.volume = volumeInputE.value/100.0;
+    if ( audioE.volume == 0 ) {
+      level = 0;
+    } else if ( audioE.volume < 0.33 ) {
+      level = 1;
+    } else if ( audioE.volume < .67 ) {
+      level = 2;
+    } else  {
+      level = 3;
+    }
+    volumeIconE.src = './assets/icons/volume-level-' +level + '.svg';
+  });
 }
