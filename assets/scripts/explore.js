1bdf7abd42d330631cc4bd0f30021894e35114b5
bkim9 <bek001@ucsd.edu>
2022-10-28 20:19:39 -0700

    Update explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,56 +1,71 @@
 // explore.js
-// const synth = window.speechSynthesis;
-// let voices = [];
-window.addEventListener('DOMContentLoaded', init);
 
+window.addEventListener('DOMContentLoaded', init);
+const synth = window.speechSynthesis;
+var voices = [];
+function populateVoiceList() {
+  if (typeof speechSynthesis === 'undefined') {
+    return;
+  }
+  setTimeout(() => {
+    voices = window.speechSynthesis.getVoices();
+    for( let i = 0; i< voices.length; i++) {
+      const option = document.createElement('option');
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+      if( voices[i].default) {
+        option.textContent += ' -- DEFAULT';
+      }
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute*("data-name", voices[i].name);
+      document.getElementById('voice-select').appendChild(option);
+    }
+  }, 500);
+}
+
+function speak() {
+  if (synth.speaking) {
+    console.log('speaking');
+    return;
+  } 
+  const smileImgE = document.querySelector('header + img');
+  smileImgE.src = "/assets/images/smiling-open.png";
+  const textInputE = document.querySelector('#text-to-speak');
+  const voiceSelectE = document.querySelector('#voice-select');
+  if (textInputE.value !== "") {
+    const utterThis = new SpeechSynthesisUtterance(textInputE.value);
+    utterThis.onend = function (event) {
+      console.log("speech onend");
+    };
+    utterThis.onerror = function (event) {
+      console.error('speech onerror');
+    };
+    const selectedVoiceName = (voiceSelectE.selectedOptions[0].value.replace(/ *\([^)]*\) */g, ''));
+    for(let i=0; i< voices.length; i++) {
+      if (voices[i].name ===selectedVoiceName) {
+        console.log(voiceSelectE.selectedOptions[0].value)
+        utterThis.voice=voices[i];
+        break;
+      }
+    }
+    synth.speak(utterThis);
+  }
+}
+
+function speakingCheck() {
+  const smileImgE = document.querySelector('header + img');
+  console.log(synth.speaking);
+  smileImgE.src = (synth.speaking)? "/assets/images/smiling-open.png":"/assets/images/smiling.png";
+}
 
 function init() {
   // TODO
-  const smileImgE = document.querySelector('header + img');
-  const textInputE = document.querySelector('#text-to-speak');
+  // const smileImgE = document.querySelector('header + img');
+  // // const textInputE = document.querySelector('#text-to-speak');
   const voiceSelectE = document.querySelector('#voice-select');
   const talkButtonE = document.querySelector('select + button');
+  populateVoiceList();
 
-
-  let utterance = new SpeechSynthesisUtterance('Hellow world!');
-  speechSynthesis.speak(utterance);
-  const voices = speechSynthesis.getVoices();
-  alert(`${voices[1].name}`);
-
-
-
-  const synth = window.speechSynthesis;
-  synth.getVoices();
-  // alert(synth.getVoices())
-  for (let i =0; i< voices.length; i++) {
-    const option = document.createElement('option');
-    option.textContent = 'hello@';//`${voices.at(i).name}(${voices.at(i).lang})`;
-    if (voices.at(i).default) {
-      option.textContent += ' - DEFAULT';
-    }
-    option.setAttribute('data-lang', voices.at(i).lang);
-    option.setAttribute('data-name', voices.at(i).name);
-    voiceSelectE.appendChild(option);
-    alert(voiceSelectE);
-  }
-
-
-
-  for (let i =0; i< voices.length; i++) {
-    const option = document.createElement('option');
-    option.textContent = `${voices[i].name}(${voices[i].lang})`;
-
-    if (voices[i].default) {
-      option.textContent += ' - DEFAULT';
-    }
-    option.setAttribute('data-lang', voices[i].lang);
-    option.setAttribute('data-name', voices[i].name);
-    voiceSelectE.appendChild(option);
-    alert(voiceSelectE);
-  }
-  talkButtonE.addEventListener('click', () => {
-    let utterance = new SpeechSynthesisUtterance("Hello world!");
-    speechSynthesis.speak(utterance)
-  })
-  // voices = speechSynthesis.getVoices()
+  talkButtonE.addEventListener('click',speak)
 }
+speakingCheck();
+// smileImgE.src = "smiling" + '-open' + '.svg';
